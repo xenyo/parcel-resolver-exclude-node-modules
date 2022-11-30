4adcd306ff2f43e7d177e09a40f60da5f7496efe
Parkle Lee <parkle.lee@xenyo.com>
2022-11-30 11:56:04 +0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const { Resolver } = require('@parcel/plugin');
+
+module.exports = new Resolver({
+  async resolve({ specifier }) {
+    if (specifier.includes('node_modules/')) {
+      return { isExcluded: true }
+    }
+    return null;
+  }
+});
