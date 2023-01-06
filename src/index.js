67d89f5615c8cc33135045ff8b444d16acc0d823
Parkle Lee <parkle.lee@xenyo.com>
2023-01-06 14:03:17 +0800

    Fix for windows style slashes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const { Resolver } = require('@parcel/plugin');
 
 module.exports = new Resolver({
   async resolve({ specifier }) {
-    if (specifier.includes('node_modules/')) {
+    if (specifier.includes('node_modules')) {
       return { isExcluded: true }
     }
     return null;
